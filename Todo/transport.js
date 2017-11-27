ce025bf73996ec3895787c90e7a136b7f1384d2a
Kolawole Oyafajo <kolaoyafajo@yahoo.com>
2017-11-27 17:53:09 +0100

    ok

--- Todo/transport.js
+++ Todo/transport.js
@@ -3,8 +3,8 @@
  */
 
 $(document).ready(function () {
-    $('#input').hide();
-    $('#up').hide();
+        $('#input').hide();
+        $('#up').hide();
 
 //to add event
     $('#plus').click(function () {
@@ -24,14 +24,18 @@ $(document).ready(function () {
             dataType:'html',
             success: function(result){
 
-                if ( result="you need to enter both fields"){
+                if (result="you need to enter both fields"){
 
-                    document.getElementById('error').innerHTML = "you need to enter both fields";
+
+                    $('#error').html('you need to enter both fields');
+
+
 
                 }
+
                 if(result="reload"){
 
-                    location.reload(true);
+                    location.reload();
                 }
 
 
@@ -63,13 +67,16 @@ $(document).ready(function () {
 //deleting event
     $('[name=delete]').click(function () {
         var iden = $(this).children().eq(0).val();
-        $.post('delete.php', {the: iden}, function (result) {
-            if(result="reload"){
-                location.reload(true);
-            }
+        var confirmation= confirm('are you sure');
+        if (confirmation==true) {
+            $.post('delete.php', {the: iden}, function (result) {
+                if (result = "reload") {
+                    location.reload();
+                }
 
 
-        });
+            });
+        }
 
     });
 
@@ -91,16 +98,24 @@ function update() {
     var times = document.getElementById("time").value;
     var ids= localStorage.getItem('idd');
     xml.onreadystatechange = function () {
-        if (xml.readyState ==4 && xml.status==200) {
-            if (xml.responseText="you need to enter both fields") {
-                document.getElementById('error').innerHTML="you need to enter both fields";
+        if (xml.readyState ===4 ) {
 
-            }
+
+
             if(xml.responseText="reload"){
-                location.reload(true);
+                location.reload();
+
+
+                if (xml.responseText="you need to enter both fields") {
+                    document.getElementById('error').innerHTML=xml.responseText;
+
+                }
             }
+
+
 
         }
+
     }
     var url="update.php?event="+events+"&time="+times+"&id="+ids;
 
@@ -108,8 +123,7 @@ function update() {
     xml.open("GET", url, true);
 
 
-    xml.send();localStorage.removeItem('idd');
-
+    xml.send();
     localStorage.removeItem('idd');
 }
 
